cc3a4d63916b80d3c3df6d417157e2e50c27a291
praeterusdice <your-email@example.com>
2026-08-29 14:11:27 -0400

    added import and route for messages in router ts

--- src/router.ts
+++ src/router.ts
@@ -14,6 +14,7 @@ import ForumDonationTalk from './pages/forum/donation-talk.vue'
 import ForumDiceChat from './pages/forum/dice-chat.vue'
 import Login from './pages/login.vue'
 import Register from './pages/register.vue'
+import Messages from './pages/messages.vue'
 import { authState, waitForAuthReady } from './services/authStore'
 
 const routes: Array<RouteRecordRaw> = [
@@ -25,6 +26,7 @@ const routes: Array<RouteRecordRaw> = [
   { path: '/current-campaign', name: 'CurrentCampaign', component: CurrentCampaign },
   { path: '/donate-items', name: 'DonateItems', component: DonateItems },
   { path: '/account', name: 'Account', component: Account },
+  { path: '/messages', name: 'Messages', component: Messages },
   { 
     path: '/forum', 
     name: 'Forum', 
