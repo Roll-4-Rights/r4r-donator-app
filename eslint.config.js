63510e13ff848cab3b71962601263bca96fccaef
praeterusdice <your-email@example.com>
2026-08-30 11:37:55 -0400

    added linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,12 @@
-import vuetify from 'eslint-config-vuetify'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
 
-export default vuetify()
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+]);
